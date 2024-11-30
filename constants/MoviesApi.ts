f868a1f449c987ae26423e10cfa1ef0202b44742
SallamRady <sallamrady@gmail.com>
2024-11-30 23:48:42 +0200

    set movies apis

--- constants/MoviesApi.ts
+++ constants/MoviesApi.ts
@@ -1,12 +1,15 @@
+const base_url = "https://api.themoviedb.org/3";
+export const poster_base_url = "https://image.tmdb.org/t/p/original";
+
 const moviesApis = {
-  trending: `/trending/all/week?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&language=en-US`,
-  netflixOriginals: `/discover/tv?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_networks=213`,
-  topRated: `/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&language=en-US`,
-  actionMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=28`,
-  comedyMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=35`,
-  HorrorMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=27`,
-  romanceMovies: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=10749`,
-  documentaries: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=99`,
+  trending: `${base_url}/trending/all/week?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&language=en-US`,
+  netflixOriginals: `${base_url}/discover/tv?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_networks=213`,
+  topRated: `${base_url}/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&language=en-US`,
+  actionMovies: `${base_url}/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=28`,
+  comedyMovies: `${base_url}/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=35`,
+  HorrorMovies: `${base_url}/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=27`,
+  romanceMovies: `${base_url}/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=10749`,
+  documentaries: `${base_url}/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_API_KEY}&with_genres=99`,
 };
 
 export default moviesApis;
