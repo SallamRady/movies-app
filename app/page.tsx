b2d0d728facc91624513c829044ffc1a6c0890e6
SallamRady <sallamrady@gmail.com>
2024-11-30 23:50:46 +0200

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,45 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { notFound } from "next/navigation";
+
+import MoviesGenreList from "@/components/MoviesGenreList";
+import { Flex } from "@radix-ui/themes";
+import getAllGenresMovies from "@/utils/api/getAllGenresMovies";
+
+export default async function Home() {
+  // Vars
+  const moviesGenres = await getAllGenresMovies();
+
+  if (!moviesGenres) notFound();
+
+  return (
+    <Flex direction={"column"} gap={"3"}>
+      <MoviesGenreList
+        title="Trending Now"
+        movies={moviesGenres?.trending ?? []}
+      />
+      <MoviesGenreList
+        title="Top Rated"
+        movies={moviesGenres?.topRated ?? []}
+      />
+      <MoviesGenreList
+        title="Action Movies"
+        movies={moviesGenres?.actionMovies ?? []}
+      />
+      <MoviesGenreList
+        title="Comedy Movies"
+        movies={moviesGenres?.comedyMovies ?? []}
+      />
+      <MoviesGenreList
+        title="Romance Movies"
+        movies={moviesGenres?.romanceMovies ?? []}
+      />
+      <MoviesGenreList
+        title="Documentaries Movies"
+        movies={moviesGenres?.documentaries ?? []}
+      />
+      <MoviesGenreList
+        title="HorrorMovies"
+        movies={moviesGenres?.horrorMovies ?? []}
+      />
+    </Flex>
+  );
 }
